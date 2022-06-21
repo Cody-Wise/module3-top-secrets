b3d24ded3fe10cb6fdb8933b621dd60cc5ab0d9a
Cody-Wise <cody@sharptackmedia.com>
2022-06-21 16:17:34 -0700

    Added middleware, model, controller, and userservice so I could test use login. Test passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,27 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+//Fake Account for Testing
+
+const fakeUser = {
+  email: 'test@test.com',
+  password: '123456',
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(fakeUser);
+    const { email } = fakeUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
   afterAll(() => {
     pool.end();
