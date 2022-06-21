03d66db4c70901efeb201be60bdf74e3f13a9390
Cody-Wise <cody@sharptackmedia.com>
2022-06-21 16:58:12 -0700

    Added delete route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -32,6 +32,16 @@ describe('backend-express-template routes', () => {
     expect(res.status).toEqual(200);
   });
 
+  it('Deletes session and logs out user', async () => {
+    await request(app).post('/api/v1/users').send(fakeUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@test.com', password: '123456' });
+    expect(res.status).toEqual(200);
+    const res2 = await request(app).delete('/api/v1/users/sessions');
+    expect(res2.status).toEqual(200);
+  });
+
   afterAll(() => {
     pool.end();
   });
